fef630f4f1e7580a05d93be3332424fa1682b2d4
Natan Souza <42328139+natan1506@users.noreply.github.com>
2024-01-31 17:48:32 -0300

    Feat: feat initial page orders

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './global.css'
-
-import { RouterProvider } from 'react-router-dom'
-
-import { router } from './routes'
-
-export function App() {
-  return <RouterProvider router={router} />
-}
